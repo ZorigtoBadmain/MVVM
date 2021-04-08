10ba21e0f516a028444536baa2959079f284dff1
Zorigto Badmain <badmainzzz@mail.ru>
2021-04-09 08:08:28 +0900

    MVC project first

---
+++ MVVM-1/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  MVVM-1
+//
+//  Created by Зоригто Бадмаин on 09.04.2021.
+//
+
+import Foundation
+
+struct Profile {
+    let name: String
+    let secondName: String
+    let age: Int
+}
